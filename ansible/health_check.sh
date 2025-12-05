576f897bbaa43597726420bcad0ac610cb47ade5
Ahmed Ben Makhlouf <101886914+Ahmedbm99@users.noreply.github.com>
2025-12-05 16:17:03 +0100

    ansible

---
+++ ansible/health_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 5
+
+STATUS=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:5000/actuator/health)
+
+if [ "$STATUS" == "200" ]; then
+  echo "Health OK"
+  exit 0
+else
+  echo "Health check failed"
+  exit 1
+fi
