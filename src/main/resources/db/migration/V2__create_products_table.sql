caf1f0a44a0624af09cb42f61455b4199c007c8b
Ahmed Ben Makhlouf <101886914+Ahmedbm99@users.noreply.github.com>
2025-12-06 18:28:22 +0100

    adjusting postgres con

--- src/main/resources/db/migration/V2__create_products_table.sql
+++ src/main/resources/db/migration/V2__create_products_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS products (
-    id BIGINT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description TEXT,
     price NUMERIC(10,2) NOT NULL,
